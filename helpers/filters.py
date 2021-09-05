bf5c0b8a38fbd17537fbba2b78e48bdea0e2f0e9
sahilnolia7 <68807017+sahilnolia7@users.noreply.github.com>
2021-09-05 22:28:37 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -9,9 +9,9 @@ async def FilterMessage(message: Message):
         return 200
     if (len(Config.FORWARD_FILTERS) == 9) or ((message.video and ("video" in Config.FORWARD_FILTERS)) or (message.document and ("document" in Config.FORWARD_FILTERS)) or (message.photo and ("photo" in Config.FORWARD_FILTERS)) or (message.audio and ("audio" in Config.FORWARD_FILTERS)) or (message.text and ("text" in Config.FORWARD_FILTERS)) or (message.animation and ("gif" in Config.FORWARD_FILTERS)) or (message.poll and ("poll" in Config.FORWARD_FILTERS)) or (message.sticker and ("sticker" in Config.FORWARD_FILTERS))):
         return 400
-blacklist=['hello']
-for x in blacklist:
-        if(x in message.text):
-       return 400
+        blacklist=['hello']
+        for x in blacklist:
+        if (message.forward_from or message.forward_from_chat) and (x in "forwarded"):
+        return 400
     else:
         return 200
