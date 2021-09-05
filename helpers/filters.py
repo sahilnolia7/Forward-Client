e1635bab5ee9cb66271b88eb774425aadf1cb5dd
sahilnolia7 <68807017+sahilnolia7@users.noreply.github.com>
2021-09-05 22:17:38 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -10,9 +10,7 @@ async def FilterMessage(message: Message):
     if (len(Config.FORWARD_FILTERS) == 9) or ((message.video and ("video" in Config.FORWARD_FILTERS)) or (message.document and ("document" in Config.FORWARD_FILTERS)) or (message.photo and ("photo" in Config.FORWARD_FILTERS)) or (message.audio and ("audio" in Config.FORWARD_FILTERS)) or (message.text and ("text" in Config.FORWARD_FILTERS)) or (message.animation and ("gif" in Config.FORWARD_FILTERS)) or (message.poll and ("poll" in Config.FORWARD_FILTERS)) or (message.sticker and ("sticker" in Config.FORWARD_FILTERS))):
         return 400
 blacklist=['hello']
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    for x in blacklist:
+for x in blacklist:
         if(x in message.text):
        return 400
     else:
